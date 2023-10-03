09d62f5423762dc07c11381833ae2cc360ba7ffd
MrvZzZzZ <126238046+MrvZzZzZ@users.noreply.github.com>
2023-10-03 18:03:10 +0300

    Add files via upload

---
+++ H_output.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <fstream>
+#include <iostream>
+#include <regex>
+
+using namespace std;
+
+void saveTheArray(int* array);
+string Check_string();
+bool Is_valid_filename(string& Filename);
+bool Is_only_read(string Filename);
+bool Is_file_exists(string Filename);
+
